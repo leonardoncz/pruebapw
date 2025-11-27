eff0ba059c55e350dc3c760f6576be0cc155870a
leonardoncz <leonardo75@gmail.com>
2025-11-26 19:37:52 -0500

    arreglar error env

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,6 +3,7 @@
   const fs = require("fs");
   const path = require("path");
   const Sequelize = require("sequelize");
+  const env = process.env.NODE_ENV || 'development';
   const config = require("../config/config.js")[env];
 
   const db = {};
